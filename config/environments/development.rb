4de2e9ba13741478d96f44d52a4a5d92026caf60
kosukecoulomb <jobch.coulomb@gmail.com>
2022-05-14 07:07:16 +0000

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "7c00853f6b6c4a119007d98d5d8b637f.vfs.cloud9.us-east-1.amazonaws.com"
 end
