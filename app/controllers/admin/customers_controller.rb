da0d9b81ae5118d5148a31c6696c5e9cd3ac17ac
fukutaniryousuke <Fr83653554@gmail.com>
2022-05-17 09:39:27 +0000

    [add]admin/customerのedit作りました

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,6 +14,17 @@ class Admin::CustomersController < ApplicationController
 
   def update
     @customer = Customer.find(params[:id])
-    
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postcode, :address, :tel, :email, :is_deleted)
+  end
+
 end
