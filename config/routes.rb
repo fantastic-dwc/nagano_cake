e57043dcb36dfca5712720e64e56e9e922b06791
kosukecoulomb <jobch.coulomb@gmail.com>
2022-05-17 04:40:45 +0000

    [add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,32 @@ Rails.application.routes.draw do
 
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
-    sessions:'public/sessions'
+    sessions: 'public/sessions'
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: "public/homes#top"
+  get "/about" => "public/homes#about", as: "about"
+  resources :products, only:[:index,:show]
+  resources :customers, only:[:show,:edit,:update] do
+    get "/customers/unsubscribe" => "public/customers#unsubscribe", as: "unsubscribe"
+    patch "/customers/withdrawal" => "public/customers#withdrawal", as: "withdrawal"
+  end
+  resources :cart_products, only:[:index,:create,:update,:destroy] do
+    delete "/cart_products/destroy_all" => "public/cart_products#destroy_all", as: "destroy_all"
+  end
+  resources :orders, only:[:index,:show,:new,:create] do
+    post "/orders/confirm" => "public/orders#confirm", as: "confirm"
+    get "/orders/complete" => "public/orders#complete", as: "complete"
+  end
+  resources :shipping_addresses, only:[:index,:edit,:create,:update,:destroy]
+  
+  namespace :admin do
+    resources :products, only:[:index,:new,:show,:edit,:create,:update]
+    resources :genres, only:[:index,:edit,:create,:update]
+    resources :customers, only:[:index,:show,:edit,:update]
+    resources :orders, only:[:show,:update]
+    resources :order_products, only:[:update] 
+    get "/admin" => "admin/homes#top", as: "admin"
+  end
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
