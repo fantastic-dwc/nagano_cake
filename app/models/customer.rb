12ba0fe4ff4400ee186416d8692449cd7ae4aa42
Chikennannbann <102850169+Chikennannbann@users.noreply.github.com>
2022-05-18 10:09:10 +0000

    [add]public/orders new画面 radio_button

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,12 +3,13 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :cart_products, dependent: :destroy
   has_many :products, through: :cart_products
-  
-  
- 
+  has_many :orders, dependent: :destroy
+
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
@@ -16,6 +17,6 @@ class Customer < ApplicationRecord
   validates :postcode, presence: true
   validates :address, presence: true
   validates :tel, presence: true
-  
-  
+
+
 end
