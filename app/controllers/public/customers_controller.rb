44c93da9b812f1e41f2998a9b02936e1d671f88d
kosukecoulomb <jobch.coulomb@gmail.com>
2022-05-20 05:08:58 +0000

    [add]マイページ・会員情報編集ページ追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,8 +4,20 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to customer_path(current_customer)
+    else
+      render :edit
+    end
+  end
+  
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postcode,:address,:tel)
   end
 end
