c1b0d6c0387dfeee1164d97578ac5cd657585399
kosukecoulomb <jobch.coulomb@gmail.com>
2022-05-20 04:01:21 +0000

    [add]マイページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get "/about" => "public/homes#about", as: "about"
   resources :products, only:[:index,:show], controller: "public/products"
   
-  resources :customers, only:[:show,:edit,:update] do
+  resources :customers, only:[:show,:edit,:update], controller: "public/customers" do
     get "/customers/unsubscribe" => "public/customers#unsubscribe", as: "unsubscribe"
     patch "/customers/withdrawal" => "public/customers#withdrawal", as: "withdrawal"
   end
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
     get "/orders/complete" => "public/orders#complete", as: "complete"
   end
   
-  resources :shipping_addresses, only:[:index,:edit,:create,:update,:destroy]
+  resources :shipping_addresses, only:[:index,:edit,:create,:update,:destroy], controller: "public/shipping_addresses"
   
   namespace :admin do
     resources :products, only:[:index,:new,:show,:edit,:create,:update]
