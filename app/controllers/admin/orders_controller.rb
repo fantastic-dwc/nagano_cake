13c3bd7fbbda157b98d5d7f529fe2e6e7c9fbbb5
Chikennannbann <102850169+Chikennannbann@users.noreply.github.com>
2022-05-21 02:37:36 +0000

    [add]admin/order view

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,7 @@ class Admin::OrdersController < ApplicationController
   
   def show
     @order = Oder.find(params[:id])
+    @order_producs = @order.order_producs
   end
   
   def update
--- app/models/product.rb
+++ app/models/product.rb
@@ -16,7 +16,8 @@ class Product < ApplicationRecord
 
   validates :name, presence: true
   validates :introduction, presence: true
-  validates :genre_id, presence: true
   validates :price, presence: true
+  validates :product_image, presence: true
+  validates :is_active, presence: true
 
 end
