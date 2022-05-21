604e588789c8c81dfdf2164937be8d3754393bb1
kosukecoulomb <jobch.coulomb@gmail.com>
2022-05-21 07:48:06 +0000

    [update]toppupe-ji quit exit

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     @genres = Genre.all
+    @new_products = params[:name].present? ? Genre.find(params[:name]).products : Product.where(is_active: true).limit(4).order(Arel.sql(" created_at DESC "))
   end
 
   def about
