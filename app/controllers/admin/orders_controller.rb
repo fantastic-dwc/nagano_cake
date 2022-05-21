3d6df4412835065771f0b2aeacb96a99f2d2b249
Chikennannbann <102850169+Chikennannbann@users.noreply.github.com>
2022-05-21 04:59:54 +0000

    [add]admin/order ステータス

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,21 @@
 class Admin::OrdersController < ApplicationController
-  
+
   def show
     @order = Oder.find(params[:id])
     @order_producs = @order.order_producs
   end
-  
+
   def update
-    
+    @order = Order.find(params[:id])
+    @product.update(order_params)
+    @order_producs = @order.order_producs
+    @product.update
+    redirect_to request.referer
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 end
