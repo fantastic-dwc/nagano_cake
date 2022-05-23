1e3cc11d0dc459fab2523eb4067c1a652cdd7352
fukutaniryousuke <Fr83653554@gmail.com>
2022-05-23 12:05:33 +0000

    [add]admin/order編集しました

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,11 +8,15 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order.update(order_params)
+    if @order.update(order_params)
+      @order.order_products.update_all(production_status: 1) if @order.order_status == "waiting"
+      @order.update_all(order_status: 2) if @order.order_products.production_status == "manufacturing"
+      @order.update_all(order_params: 3) if @order.order_products.production_status == "finish"
     # @order_products = @order.order_products
     # @product.update
     flash[:notice] = "更新完了しました"
     redirect_to request.referer
+    end
   end
 
   private
