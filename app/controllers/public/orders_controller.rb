dda9a51fd55e8393a85a81d862d6137648f73b7d
fukutaniryousuke <Fr83653554@gmail.com>
2022-05-23 13:17:48 +0000

    [add]色々変えました

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,14 +1,13 @@
 class Public::OrdersController < ApplicationController
   def index
-    @orders = Order.all
   end
 
   def show
-    @order = Order.find(params[:id])
   end
 
   def new
     @order = Order.new
+
   end
 
   def create
