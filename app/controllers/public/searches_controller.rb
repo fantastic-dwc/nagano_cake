475fb502edfa1504cfa4ede5ac9a5a92e8914615
Shinichiro28 <s.erk.ikt@gmail.com>
2022-05-23 15:31:50 +0000

    [edit]ヘッダーに検索欄追加　まだ使えません

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,4 +1,8 @@
 class Public::SearchesController < ApplicationController
+  before_action :authenticate_user!
+
   def search
+    @range = params[:range]
+    @products = Product.looks(params[:search], params[:word])
   end
 end
