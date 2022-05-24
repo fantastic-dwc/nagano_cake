236eb7f785203793efd2d9edcd312e238895cde3
Chikennannbann <102850169+Chikennannbann@users.noreply.github.com>
2022-05-24 09:52:35 +0000

    [add]制作ステータス連動

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -1,16 +1,23 @@
 class Admin::OrderProductsController < ApplicationController
-  
+
   def update
-    @order_products = OrderProduct.find(params[:id])
-    @order_products.update(order_product_params)
+    @order_product = OrderProduct.find(params[:id])
+    @order_product.update(order_product_params)
+    if @order_product.production_status == "manufacturing"
+      @order_product.order.update(order_status: "making")
+    end
+    order = @order_product.order
+    if order.order_products.where(production_status: "finish").count == order.order_products.count
+      order.update(order_status: "preparing")
+    end
     redirect_to request.referer
     flash[:notice] = "更新完了しました"
   end
-  
+
   private
-  
+
   def order_product_params
     params.require(:order_product).permit(:production_status)
   end
-  
+
 end
